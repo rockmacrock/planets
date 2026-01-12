5b70f82ccf3ac7bb757208469d58040971e211df
rockmacrock <femi_abim@yahoo.com>
2026-01-12 12:38:29 -0500

    Add trial.py

---
+++ trial.py
@@ -0,0 +1,9 @@
+import tempfile
+from pathlib import Path
+import numpy as np
+import pytest
+import torch
+from ray.train.torch import get_device
+from madewithml import utils
+
+# ... (rest of your script content) ...
